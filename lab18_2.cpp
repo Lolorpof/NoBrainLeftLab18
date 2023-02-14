e98db1fb2cefd757123e851be770b0442c1ceb33
Lolorpof <lolorpof@gmail.com>
2023-02-14 16:35:44 +0700

    lab18_2 & 3 completed

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
 
-________ myString(_____________________){
-	_______ = new ____________;    
-	for(int i = 0; i < N;i++) ____________ = 'A'+i;
-	__________ = 0;
+void myString(char *&ptr, int N){
+	ptr = new char[N];   
+	for(int i = 0; i < N;i++) *(ptr + i) = 'A'+i;
+	ptr[N] = 0;
 }
 
 int main(){
